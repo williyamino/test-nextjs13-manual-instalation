2cf3ac73d00af2ae9b4c8cde06a3b3e01d0ab1ce
VasylVasylyshyn <vasylyshyn1984@gmail.com>
2023-09-29 09:43:14 +0300

    Basic Project Structure

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <header>[header]</header>
+        <main>{children}</main>
+        <footer>[footer]</footer>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <>
+      <h1>My first Next.js page</h1>
+      <p>This is your first home library</p>
+    </>
+  );
+}
