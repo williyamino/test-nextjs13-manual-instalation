ccf749206e07912a8057c56bb7ab74357c149c44
VasylVasylyshyn <vasylyshyn1984@gmail.com>
2023-09-29 12:44:44 +0300

    Added css file to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
 import type { ReactNode } from "react";
+
+import "./global.css";
 
 interface LayoutProps {
   children: ReactNode;
@@ -8,9 +11,30 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
       <body>
-        <header>[header]</header>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/reviews">Reviews</Link>
+              </li>
+              <li>
+                <Link href="/about" prefetch={false}>
+                  About
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <main>{children}</main>
-        <footer>[footer]</footer>
+        <footer>
+          Game data and images courtesy of{" "}
+          <a href="https://rawg.io" target="_blank">
+            RAWG
+          </a>
+        </footer>
       </body>
     </html>
   );
--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function ReviewPage() {
   return (
     <>
       <h1>Review Page</h1>
+      <ul>
+        <li>
+          <Link href="/reviews/hollow-knight">Hollow Knight</Link>
+        </li>
+        <li>
+          <Link href="/reviews/stardew-valley">Stardew Valley</Link>
+        </li>
+      </ul>
     </>
   );
 }
