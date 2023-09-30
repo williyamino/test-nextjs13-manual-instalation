bfb190eb6ef5e7c2cec1bbf3eb9afa394b988d7c
VasylVasylyshyn <vasylyshyn1984@gmail.com>
2023-09-30 11:18:16 +0300

    Added tailwind css to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import type { ReactNode } from "react";
 
-import "./global.css";
+import "./globals.css";
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,10 +10,10 @@ interface LayoutProps {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col px-4 py-2 min-h-screen">
         <header>
           <nav>
-            <ul>
+            <ul className="flex gap-2">
               <li>
                 <Link href="/">Home</Link>
               </li>
@@ -28,8 +28,8 @@ export default function RootLayout({ children }: LayoutProps) {
             </ul>
           </nav>
         </header>
-        <main>{children}</main>
-        <footer>
+        <main className="grow py-3">{children}</main>
+        <footer className="text-center text-xs border-t py-3">
           Game data and images courtesy of{" "}
           <a href="https://rawg.io" target="_blank">
             RAWG
